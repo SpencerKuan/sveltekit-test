5bcc7f1e359ad5dadfb57c2dbcfc6b61206a9935
SpencerKuan <spencer.kuan.co@gmail.com>
2023-01-08 12:50:14 -0800

    ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
+<script>
+    var name = "bob";
+    var age = 2;
+
+    let number = 2;
+
+    function add(){
+        number = number + 1;
+        console.log(window);
+    }
+
+    import Nav from "./Nav.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Nav></Nav>
+
+<h2>My name is {name}</h2>
+<p>I am {age} years old</p>
+
+
+<button on:click={add}>Clicked {number} times</button>
+<ul>
+    {#each Array(number).fill(0).map((x, i) => i) as i}
+        <li>Item {i}</li>
+    {/each}
+</ul>
---
+++ src/routes/Nav.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { each } from "svelte/internal";
+
+    export const links = {
+        about: "hi",
+        contact: "contact",
+        projects: "projects"
+    };
+</script>
+
+<div>
+    <h1>links go here:</h1>
+
+    {#each Object.entries(links) as [text, url]}
+        <p>{text}</p>
+    {/each}
+</div>
